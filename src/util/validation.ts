cd950e96f8183aad09f91816a3c840118ff8b250
hiroki <huchida0622@gmail.com>
2022-11-09 17:33:12 +0900

    deleted:    dist/app.js 	deleted:    dist/app.js.map 	new file:   dist/bundle.js 	new file:   dist/bundle.js.map 	modified:   index.html 	modified:   src/app.ts 	new file:   src/components/base-component.ts 	new file:   src/components/project-input.ts 	new file:   src/components/project-item.ts 	new file:   src/components/project-list.ts 	new file:   src/decorators/autobind.ts 	new file:   src/models/drag-drop.ts 	new file:   src/models/project.ts 	new file:   src/state/project-state.ts 	new file:   src/util/validation.ts 	modified:   tsconfig.json

---
+++ src/util/validation.ts
@@ -0,0 +1,33 @@
+namespace App {
+  /** Validation */
+  export interface Validatable {
+    value: string;
+    required?: boolean;
+    minLength?: number;
+    maxLength?: number;
+    min?: number;
+    max?: number;
+  }
+
+  export function validate(validatableInput: Validatable): boolean {
+    let isValid = true;
+    const { value, required, minLength, maxLength, min, max } =
+      validatableInput;
+    if (required) {
+      isValid = isValid && value.trim().length > 0;
+    }
+    if (minLength) {
+      isValid = isValid && value.length >= minLength;
+    }
+    if (maxLength) {
+      isValid = isValid && value.length <= maxLength;
+    }
+    if (min) {
+      isValid = isValid && +value >= min;
+    }
+    if (max) {
+      isValid = isValid && +value <= max;
+    }
+    return isValid;
+  }
+}
